3735cb7c8602693502dbd6761af1c39144f9d427
JimwellMangulabnan <mangulabnanjimwell22@gmail.com>
2022-11-12 22:30:03 +0800

    Add Program

---
+++ Dictionary.py
@@ -0,0 +1,10 @@
+# Display a menu 
+print()
+print("#######################")
+print("#        MENU         #")
+print("# --------------------#")
+print("#   1 >> Add an item  #")	
+print("#   2 >> Search       #")
+print("#   3 >> Exit         #")	 
+print("#######################")
+print()
